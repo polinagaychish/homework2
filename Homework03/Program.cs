61c42e13f6847cc0d382f099885279c8dc7b26fb
Polina Gaychish <polina.gaychish@gmail.com>
2023-05-04 19:28:35 +0300

    work03

---
+++ Homework03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и поверяет, является ли
+// этот день выходным.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 6)
+{
+    Console.WriteLine($"{num} -> нет");
+}
+else
+{
+Console.WriteLine($"{num} -> да");
+}
