9ea3b4bc2c66cab093f3b0cfb403f0eda25cad09
Polina Gaychish <polina.gaychish@gmail.com>
2023-05-04 19:18:36 +0300

    work02

---
+++ Homework02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+
+if(num < 100)
+{
+    Console.WriteLine("нет третьей цифры");
+}
+else
+{
+    int a1 = num % 1000;   
+    int a2 = a1 / 100;
+    Console.WriteLine($"Трехзначное число {num} -> {a2}");
+}
